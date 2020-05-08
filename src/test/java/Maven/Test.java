fe4625f4b3c1c6c721b62d2b3c6b9f69bfe8d8d4
Sangeetha <m.sangeetha244@gmail.com>
2020-05-08 11:47:07 +0530

    Adding my prject to git for the first time

---
+++ src/test/java/Maven/Test.java
@@ -0,0 +1,20 @@
+package Maven;
+
+import org.testng.Assert;
+
+public class Test {
+  
+public static void main(String args[])
+{
+	
+	
+	
+	
+		System.out.println("Add");
+		int a=1;
+		int b=2;
+		Assert.assertEquals(a+b, 3);
+	
+	
+}
+}
